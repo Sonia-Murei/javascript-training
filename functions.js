55b03c826f7a19ee437da566de9830a7e74ef93e
Sonia-Murei <mureisonia@gmail.com>
2026-05-20 19:40:18 +0300

    Functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+let x = 5
+let y = 10
+let z = x + y //15
+
+
+function add_values(){
+    let x = 5
+    let y = 10
+    return x + y //15
+}
+
+// Calling
+let sum = add_values()
+console.log(sum)
